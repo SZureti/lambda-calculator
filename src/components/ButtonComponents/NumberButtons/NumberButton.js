1dd71bd7267cbadb4ecc9245bd5294120d8b625d
SAIPHER ZURETI <saipherzureti@gmail.com>
2019-07-16 14:25:47 -0500

    Finished Numbers and Number Button

--- src/components/ButtonComponents/NumberButtons/NumberButton.js
+++ src/components/ButtonComponents/NumberButtons/NumberButton.js
@@ -1,9 +1,12 @@
 import React from "react";
 
-const NumberButton = () => {
+const NumberButton = (props) => {
+  console.log("NUMBER BUTTON: ", props.changeData);
   return (
     <>
       {/* Display a button element rendering the data being passed down from the parent container on props */}
+      <button>{props.number}</button>
     </>
   );
 };
+export default NumberButton;
--- src/components/ButtonComponents/NumberButtons/Numbers.js
+++ src/components/ButtonComponents/NumberButtons/Numbers.js
@@ -1,16 +1,23 @@
-import React from "react";
-
+// import { numberLiteralTypeAnnotation } from "@babel/types";
+import React, { useState } from 'react';
 //import any components needed
+import { numbersData } from '../../../data';
+import NumberButton from './NumberButton';
 
 //Import your array data to from the provided data file
 
 const Numbers = () => {
   // STEP 2 - add the imported data to state
+  const [numberState, setNumberState] = useState(numbers);
   return (
     <div>
       {/* STEP 3 - Use .map() to iterate over your array data and return a button
        component matching the name on the provided file. Pass
        it any props needed by the child component*/}
+       {numbers.map(number => (
+         <NumberButton number={number}/>
+       ))}
     </div>
   );
 };
+export default Numbers;
